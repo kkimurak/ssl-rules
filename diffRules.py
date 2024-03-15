e9079cd9f4ee7a96f4e1b5198efd08cd959d58e2
Kazunori Kimura <norytama.k2@gmail.com>
2024-03-15 22:15:02 +0900

    Revert "ci: install htmldiffer from github@develop branch, specify file encoding"
    This reverts commit aa88b94e472c3dc88610179e660a81f4773c9532.

--- diffRules.py
+++ diffRules.py
@@ -1,7 +1,5 @@
-import io, sys
+import sys
 from htmldiffer import diff
-
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 try:
     a, b = sys.argv[1:3]
@@ -9,6 +7,6 @@ except ValueError:
     print("htmldiffer: highlight the differences between two html files")
     print("usage: " + sys.argv[0] + " a b")
     sys.exit(1)
-d = diff.HTMLDiffer(html_a=a, html_b=b, encoding='utf-8')
+d = diff.HTMLDiffer(a, b)
 
 print(d.combined_diff)
